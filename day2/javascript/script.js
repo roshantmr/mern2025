ecd407ab3880049001f0362e8b65f9647ad162ff
ROSHAN TMR <155134552+roshantmr@users.noreply.github.com>
2025-06-19 09:02:10 +0530

    Add files via upload

---
+++ day2/javascript/script.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JavaScript Arrays</title>
+</head>
+<body>
+  <h2>Open Console to See Array Outputs</h2>
+
+  <script>
+    // String array
+    let fruits = ["Apple", "Banana", "Mango"];
+    
+    // Number array
+    
+    let numbers = [10, 20, 30, 40];
+    
+    // Mixed type array
+    let mixed = [42, "Hello", true, null];
+    
+    // Array of objects
+    let students = [
+      { name: "Roshan", age: 21 },
+      { name: "Anu", age: 22 }
+    ];
+    
+    // Display arrays in console
+    console.log("Fruits:", fruits);
+    console.log("Numbers:", numbers);
+    console.log("Mixed:", mixed);
+    console.log("Students:", students);
+  </script>
+</body>
+</html>
